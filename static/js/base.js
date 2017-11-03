dbeaf061c9dbaf7c58ef13902a46eed472bf436b
Marc <recatala.marc@gmail.com>
2017-11-03 15:10:19 +0100

    Added local jquery and worked on the cart slider style

--- static/js/base.js
+++ static/js/base.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
   $("html").click(function() {
-    $("#cart_wrapper").hide();
+    $("#cart_wrapper").slideUp();
   });
 
   $("#cart_icon").click(function(event){
     event.stopPropagation();
-    $("#cart_wrapper").toggle();
+    $("#cart_wrapper").slideToggle();
   });
 
   $("#cart_wrapper").click(function(event)
